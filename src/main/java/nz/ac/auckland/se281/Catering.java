806bb603041a0668e8e697a235c9695a1169c069
shewho1119 <parkshihoo1119@gmail.com>
2024-04-19 13:48:34 +1200

    improve access modifiers based on the Code Style feedback - changing public to private

--- src/main/java/nz/ac/auckland/se281/Catering.java
+++ src/main/java/nz/ac/auckland/se281/Catering.java
@@ -4,14 +4,14 @@ import nz.ac.auckland.se281.Types.CateringType;
 
 public class Catering extends Service {
 
-  public CateringType cateringType;
+  private CateringType cateringType;
 
   public Catering(String bookingReference, CateringType cateringType) {
     super(bookingReference, cateringType.getName());
     this.cateringType = cateringType;
   }
 
-  public int cost;
+  private int cost;
 
   public int getCost() {
     cost = cateringType.getCostPerPerson();
--- src/main/java/nz/ac/auckland/se281/FloralSystem.java
+++ src/main/java/nz/ac/auckland/se281/FloralSystem.java
@@ -4,7 +4,7 @@ import nz.ac.auckland.se281.Types.FloralType;
 
 public class FloralSystem extends Service {
 
-  public FloralType floralType;
+  private FloralType floralType;
 
   public FloralSystem(String bookingReference, FloralType floralType) {
     super(bookingReference, floralType.getName());
